8b5d423b9b1ab60393396edba6500f3422d51916
jfearnow <jfearnow79@gmail.com>
2019-03-05 14:26:14 -0500

    created post gui in admin.py file, in models created a __str__, in views i refactored the code, in home.html updated the date display

--- instagram/views.py
+++ instagram/views.py
@@ -1,28 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'JonF',
-        'title': 'blog post 1',
-        'content': 'Fist Instagram post',
-        'date_posted': 'March 4, 2019'
-    },
-    {
-        'author': 'ChaiG',
-        'title': 'blog post 2',
-        'content': 'Fist Instagram post',
-        'date_posted': 'March 3, 2019'
-
-
-    }
-
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'instagram/home.html', context)
 
