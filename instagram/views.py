7455f3c4b7969fdce880745c682f4a20a9ee6a3f
jfearnow <jfearnow79@gmail.com>
2019-03-07 11:07:28 -0500

    created ability for user to make new post

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView
+)
 from .models import Post
 
 
@@ -9,6 +13,25 @@ def home(request):
     }
     return render(request, 'instagram/home.html', context)
 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'instagram/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+# TODO: change name of show route name
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+     
 
 def about(request):
     return render(request, 'instagram/about.html', {'title': 'About'})
