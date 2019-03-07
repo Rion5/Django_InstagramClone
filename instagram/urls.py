e1bb20e994845380f6e58e8c6d9a320f5cdc90e1
jfearnow <jfearnow79@gmail.com>
2019-03-07 12:44:15 -0500

    this is all the files i worked in for users being able to update and delete a post. also being able to create new post.

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,7 +3,8 @@ from .views import (
     PostListView, 
     PostDetailView, 
     PostCreateView,
-    PostUpdateView
+    PostUpdateView,
+    PostDeleteView
 )
 from . import views
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='instagram-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='instagram-about'),
 ]
