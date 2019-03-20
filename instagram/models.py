139fd07f83d9e1bcd08ab6f5f4eb311520dc55ba
Rion5 <31965265+Rion5@users.noreply.github.com>
2019-03-19 23:39:23 -0400

    Remove excess code

--- instagram/models.py
+++ instagram/models.py
@@ -11,9 +11,6 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='content_pics')
 
-    # def __str__(self):
-    #     return f'{self.user.username} Post'
-
     def save(self, **kwargs):
         super().save(**kwargs)
 
@@ -30,5 +27,3 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post-detail", kwargs={"pk": self.pk})
-    
-
