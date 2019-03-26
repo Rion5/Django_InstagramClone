abc2149699f43810d8300d122b8fd86e007ea344
jfearnow <jon.fearnow@sapper6dev.tech>
2019-03-26 13:58:48 -0400

    Prelaunch push

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -130,7 +130,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'instagram-home'
 LOGIN_URL = 'login'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
+try:
+    from .local_settings import *
+except ImportError:
+    pass
